c90dcd4401036d9ef7ae8b029388a244f9f241d1
Alex Lach <alexglach@gmail.com>
2016-07-07 12:41:46 -0400

    add game file

--- lib/alexvishalrps.rb
+++ lib/alexvishalrps.rb
@@ -1,6 +1,7 @@
 require "alexvishalrps/version"
 require "alexvishalrps/player"
 require "alexvishalrps/comp_player"
+require "alexvishalrps/game"
 
 module AlexVishalRPS
   def self.play
---
+++ lib/alexvishalrps/game.rb
@@ -0,0 +1,23 @@
+class Game
+
+  def initialize
+  end
+
+  def play
+    assign_players
+    quit = false
+
+    until quit
+      @player_one.gather_choice
+      @player_two.gather_choice
+      print_choices
+      determine_winner
+
+      puts "Play again?"
+      choice = gets.chomp
+
+      if choice.downcase != "yes"
+        quit = true
+      end
+    end
+  end
